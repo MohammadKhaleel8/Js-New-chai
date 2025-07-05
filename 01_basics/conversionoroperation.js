cdeb4d2b0875bcb822d8981a8f7edc1f14aac54c
MohammadKhaleel8 <mohammadkhaleelshaik8@gmail.com>
2025-07-05 16:35:24 +0000

    operations

--- 01_basics/conversionoroperation.js
+++ 01_basics/conversionoroperation.js
@@ -23,4 +23,38 @@ console.log(stringNumber);
 console.log(typeof stringNumber);
 
 
+// operation ///
 
+let str1 = "kimi";
+
+let str2 = " khaleel"
+
+let str3 = console.log(str1 + str2);
+
+console.log( "1" + 3);
+
+console.log(1 + "2");
+
+console.log( 1 + 2 + "3");
+
+console.log("1" + "2");
+
+
+console.log(+true);
+
+console.log(+"");
+
+
+//prefix and post fix
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
