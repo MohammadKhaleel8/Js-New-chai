0889577699c6f85dcbc2da44decbbed78202dc34
MohammadKhaleel8 <mohammadkhaleelshaik8@gmail.com>
2025-07-05 16:10:15 +0000

    conersionofdatatypes

---
+++ 01_basics/conversionoroperation.js
@@ -0,0 +1,26 @@
+//Important topic
+
+// "33" => 33
+
+// "33abc"  => NaN
+
+//true => 1, false =>0
+
+
+let isLoggedIn = 1
+
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedin);
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
