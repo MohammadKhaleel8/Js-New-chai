09519031c3c62e013c5173510f095b398c986134
MohammadKhaleel8 <mohammadkhaleelshaik8@gmail.com>
2025-07-06 09:46:30 +0000

    strings methods

---
+++ 01_basics/string.js
@@ -0,0 +1,52 @@
+// const empName = "khaleel";
+
+// //console.log(empName);
+
+
+// const newstring = empName.charAt(2);
+
+// const newstringsnother = empName.indexOf('l');
+
+// console.log(newstringsnother);
+
+// const newsubstring  = empName.substring(0,5);
+
+
+// console.log(newsubstring);
+
+// const newinputempname = "   khaleel    "
+
+// console.log(newinputempname.trim());
+
+
+const newurl = "http://www.google.com/mohammad%20khaleel"
+
+console.log(newurl.replace('%20','-'));
+
+
+console.log(newurl.includes('khaleel'));
+
+
+const gameName = "khaleel-ts-hello-world"
+
+
+console.log(gameName.split('-'));
+
+const str = "The quick brown fox jumps over the lazy dog.";
+
+const words = str.split(" ");
+
+
+console.log(words[3]);
+// Expected output: "fox"
+
+const chars = str.split("");
+console.log(chars[8]);
+// Expected output: "k"
+
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
+
+
+
