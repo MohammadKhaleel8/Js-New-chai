738a0bb467b5654c4c9cfd2bae02f5852585183a
MohammadKhaleel8 <mohammadkhaleelshaik8@gmail.com>
2025-07-06 10:36:01 +0000

    numbersandmaths

---
+++ 01_basics/numsandmaths.js
@@ -0,0 +1,80 @@
+// const score = 400
+
+// console.log(score); // 400
+
+
+// const balance = new Number(300);
+
+// console.log(balance);   //[Number : 300]
+
+
+// const convertedbalance = balance.toString();
+
+// console.log(convertedbalance);
+
+// console.log(typeof convertedbalance);
+
+// console.log(score.toFixed(2));
+
+// const myNumber = 1123.89323;
+
+
+// console.log(myNumber.toPrecision());
+
+
+// const myTotalBalance  = 40000230
+
+// console.log(myTotalBalance.toLocaleString('en-IN'));
+
+
+
+///++++++++++++++++++++++++ Maths +++++++++++++++++++++++++/////////////
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(4.33));
+
+// console.log(Math.floor(4.33));
+
+// console.log(Math.ceil(4.1));
+
+
+// console.log(Math.max(1,3,5,122,3234,422));
+
+// console.log(Math.min(1,3,5,122,3234,422));
+
+// console.log(Math.random()); // comes in between 0 and 1
+
+
+// console.log(Math.random()*10);
+
+
+//console.log(Math.floor(Math.random()*10 + 1));
+
+
+//Dice Game
+const max = 6;
+
+const min = 1;
+
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
