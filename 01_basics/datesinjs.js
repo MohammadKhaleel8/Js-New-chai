5bef33b4d12d1b6d8e67d69770e288498956fa69
MohammadKhaleel8 <mohammadkhaleelshaik8@gmail.com>
2025-07-06 11:13:28 +0000

    dates changes

--- 01_basics/datesinjs.js
+++ 01_basics/datesinjs.js
@@ -7,11 +7,22 @@ console.log(myDate.toDateString());
 
 console.log(myDate.toLocaleString());
 
-
 console.log(typeof myDate.toLocaleString());
-
 
 console.log(typeof myDate);
 
 
+let myCreateDate = new Date(2023,0,23) //month will start from 0 in JS
+
+console.log(myCreateDate.toDateString());
+
+
+
+let myNewCreateDate = new Date(2023,0,23,6,2);
+
+console.log(myNewCreateDate.toDateString());
+
+console.log(myNewCreateDate.toLocaleString());
+
+console.log(myNewCreateDate.toLocaleDateString());
 
